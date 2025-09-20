c9852b7df2d1b7b735596d0af5aa2e436f061cdb
joao <joaovictormelodonascimento@gmail.com>
2025-09-19 22:44:34 -0300

    atualizando form de cadastro e adicionando verificacao de cpf

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
+from pycpfcnpj import cpf
+
+def validar_cpf(value):
+    if not cpf.validate(value):
+        raise ValidationError('CPF inválido.')
 
 class Usuario(AbstractUser):
+    ESCOLHA_SEXO = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+    )    
     TIPOS = (
         ('locador', 'Locador'),
         ('inquilino', 'Inquilino'),
@@ -10,6 +21,8 @@ class Usuario(AbstractUser):
     telefone = models.CharField(max_length=20, blank=True, null=True)
     data_nascimento = models.DateField(blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-
+    cpf = models.CharField(max_length=14, blank=True, null=True, unique=True, validators=[validar_cpf])
+    sexo = models.CharField(max_length=1, choices=ESCOLHA_SEXO, blank=True, null=True)
+    
     def __str__(self):
         return f"{self.username} - {self.tipo}"
--- backend/usuarios/serializers.py
+++ backend/usuarios/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import Usuario
 from django.contrib.auth.hashers import make_password
+from pycpfcnpj import cpf
 
 class LocadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ['id', 'username', 'email', 'telefone', 'bio', 'password']  
+        fields = ['id', 'username', 'email', 'telefone', 'bio', 'cpf', 'sexo', 'password']  
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -13,14 +14,23 @@ class LocadorSerializer(serializers.ModelSerializer):
         validated_data['tipo'] = 'locador'  
         return super().create(validated_data)
 
+    def validate_cpf(self, value):
+        if not cpf.validate(value):
+            raise serializers.ValidationError("CPF inválido.")
+        return value    
 
 class InquilinoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ['id', 'username', 'email', 'telefone', 'data_nascimento', 'password']  
+        fields = ['id', 'username', 'email', 'telefone', 'cpf', 'sexo', 'data_nascimento', 'password']  
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
         validated_data['tipo'] = 'inquilino'  
         return super().create(validated_data)
+    
+    def validate_cpf(self, value):
+        if not cpf.validate(value):
+            raise serializers.ValidationError("CPF inválido.")
+        return value    
