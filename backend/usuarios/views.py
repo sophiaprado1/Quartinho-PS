9005715018ef47dd2a6883e25ad6abafd573d858
joao <joaovictormelodonascimento@gmail.com>
2025-09-20 21:08:09 -0300

    autenticacao token jwt

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,11 +1,32 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Usuario
 from .serializers import LocadorSerializer, InquilinoSerializer
 
-class LocadorViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.filter(tipo='locador')
-    serializer_class = LocadorSerializer
+from rest_framework.permissions import IsAuthenticated
 
-class InquilinoViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.filter(tipo='inquilino')
+class AllowPostAnyReadAuthenticated(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return True
+        
+        return request.user and request.user.is_authenticated
+
+
+class LocadorViewSet(viewsets.ModelViewSet):
+    serializer_class = LocadorSerializer
+    permission_classes = [AllowPostAnyReadAuthenticated]
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Usuario.objects.none()
+        return Usuario.objects.filter(tipo='locador')
+
+class InquilinoViewSet(viewsets.ModelViewSet): 
     serializer_class = InquilinoSerializer
+    permission_classes = [AllowPostAnyReadAuthenticated]
+    def get_queryset(self):
+  
+        if not self.request.user.is_authenticated:
+            return Usuario.objects.none()
+        
+        return Usuario.objects.filter(tipo='inquilino')
